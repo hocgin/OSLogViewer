9cccef5714032284595c4bc09ad303f145412500
Q.GuoQiang <gonefish@gmail.com>
2025-06-15 22:46:10 +0800

    Optimize code.

--- Sources/OSLogViewer/OSLogStore+Utils.swift
+++ Sources/OSLogViewer/OSLogStore+Utils.swift
@@ -1,19 +1,15 @@
 import Foundation
 import OSLog
 
-extension OSLogEntryLog : Identifiable {
-
-}
-
 struct OSLogStoreFilter {
 
-    let position: OSLogPosition
+    let position: OSLogPosition?
 
     let category: String?
 
     let subsystem: String?
 
-    init(position: OSLogPosition, category: String? = nil, subsystem: String? = nil) {
+    init(position: OSLogPosition? = nil, category: String? = nil, subsystem: String? = nil) {
         self.position = position
         self.category = category
         self.subsystem = subsystem
@@ -23,25 +19,31 @@ struct OSLogStoreFilter {
 
 extension OSLogStore {
 
-    func loadLogs(for filter: OSLogStoreFilter) throws -> [OSLogEntryLog] {
+    func getLogs(for filter: OSLogStoreFilter) async throws -> [OSLogEntryLog] {
+        try await withCheckedThrowingContinuation { continuation in
+            var query = [NSPredicate]()
 
-        var query = [NSPredicate]()
+            if let subsystem = filter.subsystem {
+                query.append(NSPredicate(format: "subsystem = %@", subsystem))
+            }
 
-        if let subsystem = filter.subsystem {
-            query.append(NSPredicate(format: "subsystem = %@", subsystem))
-        }
+            if let category = filter.category {
+                query.append(NSPredicate(format: "category = %@", category))
+            }
 
-        if let category = filter.category {
-            query.append(NSPredicate(format: "category = %@", category))
-        }
+            let match = query.isEmpty ? nil : NSCompoundPredicate(andPredicateWithSubpredicates: query)
 
-        let match = query.isEmpty ? nil : NSCompoundPredicate(andPredicateWithSubpredicates: query)
+            do {
+                let logs = try getEntries(at: filter.position, matching: match).compactMap {
+                    $0 as? OSLogEntryLog
+                }
 
-        return try getEntries(at: filter.position, matching: match).compactMap {
-            $0 as? OSLogEntryLog
+                continuation.resume(returning: logs)
+            } catch {
+                continuation.resume(throwing: error)
+            }
         }
     }
-
 }
 
 extension Array where Element: Hashable {
--- Sources/OSLogViewer/OSLogViewer.swift
+++ Sources/OSLogViewer/OSLogViewer.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import OSLog
+import Combine
 
 public struct OSLogViewer: View {
 
@@ -12,40 +13,43 @@ public struct OSLogViewer: View {
     private var dismiss
 
     public var body: some View {
-        NavigationView {
-            ZStack(alignment: .topLeading) {
-                List(viewModel.filteredEntries) { entry in
-                    OSLogEntryRow(entry: entry)
-                }
-                .listStyle(.plain)
-
-                .refreshable {
-                    await viewModel.reload()
-                }
-                .searchable(text: $viewModel.searchText)
-                .overlay {
-                    if viewModel.isLoading {
-                        ProgressView()
-                    }
-                }
-
-                if viewModel.isFilterPresented {
-                    OSLogFilterView()
-                        .environmentObject(viewModel)
-                        .frame(maxHeight: 300)
-                }
+        ZStack(alignment: .topLeading) {
+            List(viewModel.displayLogs, id: \.self) { entry in
+                OSLogEntryRow(entry: entry)
             }
-            .navigationTitle("OSLog Viewer")
-            .navigationBarTitleDisplayMode(.inline)
-            .sheet(isPresented: $viewModel.isExportPresented) {
-                ActivityView(items: [viewModel.filteredEntries.map(\.composedMessage).joined(separator: "\n")])
-            }
-            .toolbar {
-                defaultToolbar
-            }
-            .task {
+            .listStyle(.plain)
+            .refreshable {
                 await viewModel.reload()
             }
+            .searchable(text: $viewModel.searchText)
+            .overlay {
+                if viewModel.isLoading {
+                    ProgressView()
+                }
+            }
+
+            if viewModel.isFilterPresented {
+                OSLogFilterView()
+                    .environmentObject(viewModel)
+                    .frame(maxHeight: 300)
+            }
+        }
+        .navigationTitle("OSLog Viewer")
+        .navigationBarTitleDisplayMode(.inline)
+        .sheet(isPresented: $viewModel.isExportPresented) {
+            ActivityView(items: [viewModel.displayLogs.map(\.composedMessage).joined(separator: "\n")])
+        }
+        .toolbar {
+            defaultToolbar
+        }
+        .task {
+            await viewModel.reload()
+        }
+        .onChange(of: viewModel.selectedCategories) { _ in
+            viewModel.updateSelectLog()
+        }
+        .onChange(of: viewModel.selectedSubsystems) { _ in
+            viewModel.updateSelectLog()
         }
     }
 
@@ -74,6 +78,120 @@ public struct OSLogViewer: View {
         }
     }
 }
+
+extension OSLogViewer {
+
+    @MainActor final class ViewModel: ObservableObject {
+
+        private let logStore: OSLogStore = try! OSLogStore(scope: .currentProcessIdentifier)
+
+        @Published var displayLogs: [OSLogEntryLog] = []
+
+        private var entries: [OSLogEntryLog] = []
+
+        @Published var searchForEntries: [OSLogEntryLog] = []
+
+        @Published var allCategories = [String]()
+
+        @Published var selectedCategories = Set<String>()
+
+        @Published var allSubsystems = [String]()
+
+        @Published var selectedSubsystems = Set<String>()
+
+        @Published var isFilterPresented = false
+
+        @Published var isLoading = false
+
+        @Published var isExportPresented = false
+
+        @Published var searchText: String = ""
+
+        private var cancellables = Set<AnyCancellable>()
+
+        init() {
+            $searchText.map { [self] text in
+                if searchText.isEmpty {
+                    entries.filter { log in
+                        selectedSubsystems.contains(log.subsystem)
+                    }.filter { log in
+                        selectedCategories.contains(log.category)
+                    }
+                } else {
+                    entries.filter { log in
+                        log.composedMessage.lowercased().contains(searchText.lowercased())
+                    }
+                }
+            }
+            .debounce(for: .milliseconds(200), scheduler: RunLoop.main)
+            .receive(on: RunLoop.main)
+            .assign(to: \ViewModel.displayLogs, on: self)
+            .store(in: &cancellables)
+        }
+
+        func reload() async {
+            isLoading = true
+
+            if let entries = try? await logStore.getLogs(for: .init()) {
+                await parseLogs(entries)
+
+                self.entries = entries
+
+                self.displayLogs = self.entries
+
+                selectAllSubsystems()
+
+                selectAllCategories()
+            }
+
+            isLoading = false
+        }
+
+        nonisolated func parseLogs(_ logs: [OSLogEntryLog]) async {
+            var tmpSubsystems = Set<String>()
+
+            var tmpCategories = Set<String>()
+
+            logs.forEach { log in
+                if !tmpSubsystems.contains(log.subsystem) && !log.subsystem.isEmpty {
+                    tmpSubsystems.insert(log.subsystem)
+                }
+
+                if !tmpCategories.contains(log.category) && !log.category.isEmpty  {
+                    tmpCategories.insert(log.category)
+                }
+            }
+
+            let allSubsystems = Array(tmpSubsystems).sorted()
+
+            let allCategories =  Array(tmpCategories).sorted()
+
+            await MainActor.run {
+                self.allSubsystems = allSubsystems
+                self.allCategories = allCategories
+            }
+        }
+
+         func updateSelectLog()  {
+            let tmp = entries.filter { log in
+                selectedSubsystems.contains(log.subsystem)
+            }.filter { log in
+                selectedCategories.contains(log.category)
+            }
+
+             displayLogs = tmp
+        }
+
+        func selectAllSubsystems() {
+            selectedSubsystems = Set(allSubsystems)
+        }
+
+        func selectAllCategories() {
+            selectedCategories = Set(allCategories)
+        }
+    }
+}
+
 
 #Preview {
     OSLogViewer()
